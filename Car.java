a43900d92f6c48eb7278e8d0894520364f50186e
Hancock <bchancock.2017@mymail.jcsu.edu>
2018-01-31 14:16:56 -0500

    first

---
+++ Car.java
@@ -0,0 +1,14 @@
+
+public class Car {
+String carBrand ;
+double price ;
+int year;
+	public Car(String carBrand1, double price2, int year3) {
+		// TODO Auto-generated constructor stub
+		carBrand = carBrand1;
+		price = price2;
+		year = year3;
+		
+	}
+
+}
---
+++ CarTest.java
@@ -0,0 +1,10 @@
+
+public class CarTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Car car = new Car ("Ford", 25000, 2003);
+System.out.println(car.carBrand + "\t" + car.price + "\t" + car.year);
+	}
+
+}
